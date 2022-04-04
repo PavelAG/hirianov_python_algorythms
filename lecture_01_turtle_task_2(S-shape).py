cbcebcb1eee6755752fc0fbff60908153189de28
PavelAG <43850386+PavelAG@users.noreply.github.com>
2022-04-04 18:08:22 +0100

    Create lecture_01_turtle_task_2(S-shape).py

---
+++ lecture_01_turtle_task_2(S-shape).py
@@ -0,0 +1,23 @@
+"""
+Draw the letter S
+"""
+import turtle
+
+turtle.shape('turtle') # defining shape
+
+turtle.forward(50)
+
+turtle.left(90)
+turtle.forward(50)
+
+turtle.left(90)
+turtle.forward(50)
+
+turtle.right(90)
+turtle.forward(50)
+
+turtle.right(90)
+turtle.forward(50)
+
+# Prevent window from closing so you can see the result.
+turtle.mainloop()
