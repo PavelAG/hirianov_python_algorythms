09e202f9f34c4dcd332fc2f27bf088e576fe8d74
PavelAG <43850386+PavelAG@users.noreply.github.com>
2022-04-05 17:53:20 +0100

    Update lecture_01_turtle_task_11(butterfly).py

--- lecture_01_turtle_task_11(butterfly).py
+++ lecture_01_turtle_task_11(butterfly).py
@@ -8,14 +8,12 @@ turtle.shape('turtle')
 
 patterns_number = 20 # number of petals
 patterns_pair = int(patterns_number/2) # calculating the number of pairs
-#angle = 360/patterns_number #identifying angle
 length = 50 #size
 
 for pair in range(patterns_pair):
     turtle.left(90)
     turtle.circle(length) # making 1st pattern(circle)
     turtle.circle(-length) # making symmetrical pattern(circle)
-    #turtle.right(angle)
     turtle.right(90)
     length += 5
 
